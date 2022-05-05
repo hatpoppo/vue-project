f869a29b33f61aac75b9d551bc80ea267ea55e00
hatpoppo <toshi.hat@gmail.com>
2022-05-06 07:29:33 +0900

    Propsの変更を検知して情報を取得

--- src/components/tabs/Home.vue
+++ src/components/tabs/Home.vue
@@ -1,12 +1,34 @@
 <script setup>
-defineProps({
+import axios from "axios";
+import { watch, reactive, ref } from "vue";
+
+const props = defineProps({
   userId: Number,
 });
+const state = reactive({
+  reminders: [],
+});
+watch(
+  () => props.userId,
+  async (newword, oldword) => {
+    axios
+      .get("/api/reminders")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        state.reminders = response.data.reminders;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+);
 </script>
 <template>
-  <div class="">home
-    {{ userId }}
-  </div>
+  <div class="">home userId:{{ props.userId }}</div>
 </template>
-<style>
-</style>
+<style></style>
--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
-import { createServer, Model } from "miragejs";
+import {
+  createServer,
+  Model,
+  hasMany,
+  belongsTo,
+  RestSerializer,
+  Factory
+} from "miragejs";
 
 export default function makeServer({ environment = "development" } = {}) {
   return createServer({
     environment,
 
     models: {
-      user: Model,
+      user: Model.extend({
+        reminders: hasMany(),
+      }),
+      reminder: Model.extend({
+        user: belongsTo()
+      })
+    },
+    factories: {
+      reminder: Factory.extend({
+        text: "Reminder"
+      })
     },
 
     seeds(server) {
       server.create("user", { name: "Bob" });
       server.create("user", { name: "Alice" });
+      server.create("reminder");
     },
 
     routes() {
@@ -19,6 +37,15 @@ export default function makeServer({ environment = "development" } = {}) {
       this.get("/users", (schema) => {
         return schema.users.all();
       });
+      this.get("/reminders", (schema) => {
+        return schema.reminders.all();
+      });
+
+      this.get('/api/users/:id/reminders', (schema, request) => {
+        let userId = request.params.id
+        let user = schema.users.find(userId)
+        return user.reminders
+      })
     },
   });
 }
