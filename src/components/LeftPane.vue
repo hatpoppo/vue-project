ad6a30984f6871696cb4de411c726c4a6a5128b4
hatpoppo <toshi.hat@gmail.com>
2022-05-05 11:29:42 +0900

    ユーザを選択したときにCSSを変更

--- src/components/LeftPane.vue
+++ src/components/LeftPane.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from "axios";
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive,ref } from "vue";
 const state = reactive({
   users: [],
 });
@@ -23,8 +23,11 @@ onMounted(() => {
 });
 const emit = defineEmits(["userAt"]);
 const getUser = (e) => {
+  selecteUser.value = e.target.getAttribute("data-id");
   emit("userAt", e.target.getAttribute("data-id"));
 };
+const selecteUser = ref(0);
+
 </script>
 <template>
   <div class="left_pane">
@@ -32,7 +35,9 @@ const getUser = (e) => {
     {{ state.count }}
     <p v-if="!state.users">Loading...</p>
     <ul v-else>
-      <li v-for="user in state.users" :key="user.id" :data-id="`${user.id}`" @click="getUser">
+      <li v-for="user in state.users" :key="user.id" :data-id="`${user.id}`" @click="getUser"
+        :class="['li-selected',{active: selecteUser ==  user.id}]"
+      >
         {{ user.name }}
       </li>
     </ul>
@@ -42,5 +47,22 @@ const getUser = (e) => {
 .left_pane {
   background-color: aquamarine;
   min-width: 200px;
+}
+
+.li-selected {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: 5px;
+}
+.li-selected:hover {
+  background: #7bf3fc;
+}
+.li-selected.active {
+  background: #06f742;
 }
 </style>
