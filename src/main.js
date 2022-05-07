20d65224c323f9daadf009ed9bcc8af562686eaf
hatpoppo <toshi.hat@gmail.com>
2022-05-07 19:56:48 +0900

    mountの順序を修正

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import makeServer from "./server";
 
-createApp(App).mount('#app')
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
+
+createApp(App).mount('#app')
